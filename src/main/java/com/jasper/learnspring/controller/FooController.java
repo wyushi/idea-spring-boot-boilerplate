c632103f3f8674403f17652418020472a9647f57
Yushi Wang <jasper.ys.wang@gmail.com>
2019-08-15 22:47:39 -0700

    required = false not working for constructor injection

--- src/main/java/com/jasper/learnspring/controller/FooController.java
+++ src/main/java/com/jasper/learnspring/controller/FooController.java
@@ -1,7 +1,6 @@
 package com.jasper.learnspring.controller;
 
 import com.jasper.learnspring.service.BarService;
-import com.jasper.learnspring.service.FooService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,23 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FooController {
 
-    private FooService fooService;
     private BarService barService;
 
-    @Autowired
-    public FooController(FooService fooService) {
-        this.fooService = fooService;
+    @Autowired(required = false)
+    public FooController(BarService barService) {
+        this.barService = barService;
     }
 
     @RequestMapping("/foo-bar")
     public String get() {
-        String name = fooService.getName();
+        String name = "Foo Service";
         String version = barService != null ? barService.getVersion() : "0.0.1";
         return String.format("%s [%s]", name, version);
-    }
-
-    @Autowired(required = false)
-    public void setBarService(BarService barService) {
-        this.barService = barService;
     }
 }
